84a2982eb880ff1ed8320ebc67945da9640b9e19
RJ <jensenrs@gmail.com>
2017-11-08 16:09:24 -0800

    validation on edit games

--- src/routes/game-routes.js
+++ src/routes/game-routes.js
@@ -5,7 +5,7 @@ const gameCtrl = require('../controllers/game-controllers')
 router.get('/', gameCtrl.getAllGames)
 router.get('/:gameId', gameCtrl.getOneGame)
 router.post('/', gameCtrl.complete, gameCtrl.createGame)
-router.put('/:gameId', gameCtrl.editGame)
+router.put('/:gameId', gameCtrl.complete, gameCtrl.editGame)
 router.delete('/:gameId', gameCtrl.destroyGame)
 
 module.exports = router
